d93abb3b997eaec5be59b09e5fa61ac29e135c50
alvar <alvar@Pelayo>
2022-12-16 13:52:15 +0100

    v3.-para refactorizar este código, primero lo seleccione, a continuación le di click derecho seleccione la opción "refactor" y la opción "introduce" y dentro de esta la opción la de "constant". A continuación seleccioné la opción de public y le puse de nombre "NUM_DIG"

--- src/main/java/es/calcularpi/CalcularPI.java
+++ src/main/java/es/calcularpi/CalcularPI.java
@@ -6,7 +6,7 @@ import java.math.MathContext;
 public class CalcularPI {
 
      public static void main(String[] args) {
-        MathContext mc = new MathContext(10);         
+        MathContext mc = new MathContext(NUM_DIG);         
         //meter en el método compute
         BigDecimal pi = new BigDecimal(0);
         BigDecimal limit = new BigDecimal(1).movePointLeft(10);
@@ -22,6 +22,7 @@ public class CalcularPI {
         System.out.println(pi.round(mc));
         //meter en el método compute
     }
+    public static final int NUM_DIG = 10;
 
     public static BigDecimal piFunction(int k, MathContext mc) {
         //meter en el método piFunction
